4bc9930f37e9f87af1032101fa5dac4bad5b2d3b
t9uo <t9uo@fresnou.org>
2023-08-31 09:03:17 -0700

    commit

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', {
+    color: "#FFFFFF",
+    textColor: "#000000"
+  });
 });
 
 
 router.post('/', function(req, res){
   res.render('index',{
-    color: req.body.colorPicker
+    color: req.body.colorPicker,
+    textColor: req.body.textColorPicker
   })
 });
 module.exports = router;
+
+router.post('/random', function(req, res){
+  res.render('index',{
+    color: generateRandomHexCode(),
+    textColor: generateRandomHexCode()
+  })
+})
+
+function generateRandomHexCode() {
+  let hexCode = "#"
+  while (hexCode.length < 7) {
+    hexCode += (Math.round(Math.random() * 15)).toString(16)
+  }
+  return hexCode
+}
